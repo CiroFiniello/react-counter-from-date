e1a830bc6a23b6cc36a8ad45c10cf83183f4a7d9
CodeKiro <kirofiniello@outlook.it>
2024-10-29 22:15:41 +0100

    Count e step vengono presi dinamicamente, i bottoni funzionano per modificare lo stato di step e count

--- src/App.jsx
+++ src/App.jsx
@@ -10,17 +10,20 @@ function App() {
 }
 
 function Counter() {
+  //utilizzo lo state per il primo bottone
+  const [step, setStep] = useState(1);
+  const [count, setCount] = useState(0);
   return (
     <div>
       <div className="passi">
-        <button>-</button>
-        <h3>Step:5</h3>
-        <button>+</button>
+        <button onClick={() => setStep((s) => s - 1)}>-</button>
+        <h3>Step:{step}</h3>
+        <button onClick={() => setStep((s) => s + 1)}>+</button>
       </div>
       <div className="passi">
-        <button>-</button>
-        <h3>Count:30</h3>
-        <button>+</button>
+        <button onClick={() => setCount((s) => s - 1)}>-</button>
+        <h3>Count:{count}</h3>
+        <button onClick={() => setCount((s) => s + 1)}>+</button>
       </div>
       <h2>30 giorni da oggi sarà giovedì 21 2025</h2>
     </div>
