1caa5be1f49c5e4d1b174350e6715c5c8955ecce
CodeKiro <kirofiniello@outlook.it>
2024-10-29 22:35:00 +0100

    adesso le dipendenze possono gestire la data come richiesto

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable no-unused-vars */
 import { useState } from "react";
 import "./App.css";
 
@@ -10,22 +11,32 @@ function App() {
 }
 
 function Counter() {
-  //utilizzo lo state per il primo bottone
+  //state
   const [step, setStep] = useState(1);
   const [count, setCount] = useState(0);
+  //variables
+  const date = new Date("october 29 2024");
+  date.setDate(date.getDate() + count);
   return (
     <div>
       <div className="passi">
         <button onClick={() => setStep((s) => s - 1)}>-</button>
-        <h3>Step:{step}</h3>
+        <h3>Step: {step}</h3>
         <button onClick={() => setStep((s) => s + 1)}>+</button>
       </div>
       <div className="passi">
-        <button onClick={() => setCount((s) => s - 1)}>-</button>
-        <h3>Count:{count}</h3>
-        <button onClick={() => setCount((s) => s + 1)}>+</button>
+        <button onClick={() => setCount((s) => s - step)}>-</button>
+        <h3>Count: {count}</h3>
+        <button onClick={() => setCount((s) => s + step)}>+</button>
       </div>
-      <h2>30 giorni da oggi sarà giovedì 21 2025</h2>
+      <span>
+        {count === 0
+          ? "Oggi è  "
+          : count > 0
+          ? ` frà ${count} giorni sarà  `
+          : `${Math.abs(count)} giorni fà era `}
+      </span>
+      <span>{date.toDateString()}</span>
     </div>
   );
 }
